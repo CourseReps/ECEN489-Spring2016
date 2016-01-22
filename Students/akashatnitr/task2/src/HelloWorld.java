86374ff3b1af58ce707e31d37b22656d6ac2db56
Akash Sahoo <akash.nitr@gmail.com>
2016-01-22 15:21:38 -0600

    task 2

---
+++ Students/akashatnitr/task2/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/* Hello World by Akash Sahoo
+ECEN - 689 Task 2 */
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
