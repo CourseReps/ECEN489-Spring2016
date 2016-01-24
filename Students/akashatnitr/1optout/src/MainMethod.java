12f1bb847a7716b6507bc4ec724c4d85eb6ebecb
Akash Sahoo <akash.nitr@gmail.com>
2016-01-24 06:48:57 -0600

    1optout assignment

---
+++ Students/akashatnitr/1optout/src/MainMethod.java
@@ -0,0 +1,13 @@
+
+public class MainMethod {
+	public static void main(String[] args) {
+		ComputerData data = new ComputerData();
+		
+		System.out.println("Java version is "+data.getJavaVersion());
+		System.out.println("Current Time is "+data.getCurrTime());
+		System.out.println("OS version is "+data.getOSVersion());
+		
+		
+
+	}
+}
