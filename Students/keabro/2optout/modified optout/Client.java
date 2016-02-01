6519e1a470e9ec73340e7716d4d5f6b347e4aac6
Keaton Brown <keabro@tamu.edu>
2016-02-01 17:09:22 -0600

    changed optout

--- Students/keabro/2optout/modified optout/Client.java
+++ Students/keabro/2optout/modified optout/Client.java
@@ -2,17 +2,18 @@
 import javax.swing.*;
 import java.io.*;
 import java.net.*;
-public class client {
+public class Client {
     public static void main (String argv[]) throws Exception {
         try{
-        String serverAddress = JOptionPane.showInputDialog();
+        String serverAddress = JOptionPane.showInputDialog("enter ip address");
         Socket client = new Socket (serverAddress , 2000);
         ObjectOutputStream os = new ObjectOutputStream(client.getOutputStream());
         ObjectInputStream is = new ObjectInputStream(client.getInputStream());
-        while(recv != "exit"){
-        String recv = is.readObject();
-        System.out.println("Received: \n" + recv);
-    }
+        String recv = "";
+        do{
+        recv = (String) is.readObject();
+        System.out.print("Received: " + recv + "\n");
+        }while(!recv.equals("exit"));
         is.close();
         os.close();
         client.close();
--- Students/keabro/2optout/modified optout/Server.java
+++ Students/keabro/2optout/modified optout/Server.java
@@ -1,18 +1,23 @@
 import java.io.*;
 import java.net.*;
 import javax.swing.*;
+import java.util.Scanner;
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(2000, 15);
             while (true) {
                 Socket socket = server.accept();
+                String input = "";
                 try {
                 ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
                 ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
-                while(input != "exit"){
-                String input = JOptionPane.showInputDialog();
+                do{
+                /**Scanner reader = new Scanner(System.in;  // Reading from System.in
+                System.out.print("Enter a message: ");
+                input = reader.next();**/
+                input = System.console().readLine("Message:");
                 os.writeObject(input);
-                }
+                }while(!input.equals("exit"));
                 os.close();
                 is.close();
                 socket.close();
