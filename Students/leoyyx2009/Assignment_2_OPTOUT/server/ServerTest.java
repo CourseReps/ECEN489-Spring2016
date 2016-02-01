f9a7c1e52e5b67dfec125d97f7ee479e121144cf
Yanxiang Yang <leoyyx2009@tamu.edu>
2016-01-31 21:14:00 -0600

    Update ServerTest.java

--- Students/leoyyx2009/Assignment_2_OPTOUT/server/ServerTest.java
+++ Students/leoyyx2009/Assignment_2_OPTOUT/server/ServerTest.java
@@ -1,5 +1,8 @@
 /**
- * Created by YYX on 1/31/16.
+ * Created by YYX on 1/30/16.
+ * ECEN689-610:SPTP: DATA ACQ EMBEDDED SYS
+ * Auther:  Yanxiang Yang
+ * Assignment2: OPT-OUT
  */
 public class ServerTest {
     public static void main( String[] args ){
