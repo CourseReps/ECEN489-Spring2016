634539200c50e9edb8e443d81451223918775143
captjacksparrowz <captjack@email.tamu.edu>
2016-02-01 14:07:02 -0600

    added task 3 and assignment 2

---
+++ Students/captjack/Java2/Java2.java
@@ -0,0 +1,52 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by tbranyon on 1/29/16.
+ */
+public class ComputerData {
+    private String classpath;
+    private String jreVendor;
+    private String jreVersion;
+    private String OS_arch;
+    private String OS_name;
+    private String OS_version;
+    private String userHome;
+    private String userName;
+    private String dateTime;
+
+    public void getInfo() {
+        classpath = System.getProperty("java.class.path");
+        jreVendor = System.getProperty("java.vendor");
+        jreVersion = System.getProperty("java.version");
+        OS_arch = System.getProperty("os.arch");
+        OS_name = System.getProperty("os.name");
+        OS_version = System.getProperty("os.version");
+        userHome = System.getProperty("user.home");
+        userName = System.getProperty("user.name");
+
+        DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date curTimeDate = new Date();
+        dateTime = fmt.format(curTimeDate);
+    }
+
+    public void print() {
+        System.out.println("Timestamp     : " + dateTime + "\n" +
+                "Classpath     : " + classpath + "\n" +
+                "JRE Vendor    : " + jreVendor + "\n" +
+                "JRE Version   : " + jreVersion + "\n" +
+                "OS Arch       : " + OS_arch + "\n" +
+                "OS Name       : " + OS_name + "\n" +
+                "OS Version    : " + OS_version + "\n" +
+                "User Home Dir : " + userHome + "\n" +
+                "Username      : " + userName
+        );
+    }
+    public static void main(String[] args) {
+        ComputerData compData = new ComputerData();
+        compData.getInfo();
+        compData.print();
+    }
+}
+
