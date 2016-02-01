c5a8aeecc14928b806ed222c629b5ea1b638bc3d
tbranyon <thomas.branyon@gmail.com>
2016-02-01 15:07:29 -0600

    Update 2optout

--- Students/tbranyon/2optout/Challenge2client/client.java
+++ Students/tbranyon/2optout/Challenge2client/client.java
@@ -4,6 +4,7 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import javax.swing.JOptionPane;
 
 /**
  * Created by tbranyon on 1/27/16.
@@ -13,7 +14,8 @@ public class client
     public static void main(String[] args)
     {
         try{
-            InetAddress addr = InetAddress.getByName("127.0.0.1");
+            String addr_str = JOptionPane.showInputDialog("Enter IP");
+            InetAddress addr = InetAddress.getByName(addr_str);
             Socket connection = new Socket(addr, 2000);
             ObjectOutputStream os = new ObjectOutputStream(connection.getOutputStream());
             String req = "get_data";
