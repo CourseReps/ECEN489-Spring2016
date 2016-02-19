d878a9b0c1d994f6b791227f874ac11870442ba2
John Lusher II <john.lusher@tamu.edu>
2016-02-19 15:16:46 -0600

    Project 1 Updates

--- Students/johnlusher/Project_1/src/RFFieldSQLDatabase.java
+++ Students/johnlusher/Project_1/src/RFFieldSQLDatabase.java
@@ -17,35 +17,13 @@
 //  --------------------------------------------------------------------------------------------------------------------
 //  Imports
 //  --------------------------------------------------------------------------------------------------------------------
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.*;
 import java.text.SimpleDateFormat;
-import java.util.Collections;
 import java.util.Properties;
 import java.util.ArrayList;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.fusiontables.Fusiontables;
-import com.google.api.services.fusiontables.Fusiontables.Query.Sql;
-import com.google.api.services.fusiontables.FusiontablesScopes;
-import com.google.api.services.fusiontables.model.Table;
-import com.google.api.services.fusiontables.model.TableList;
-
 
 //  --------------------------------------------------------------------------------------------------------------------
 //        Class:    RFFieldSQLDatabase
@@ -57,66 +35,13 @@ public class RFFieldSQLDatabase
     // Define class members
     private Connection conn = null;                                     // MySql Database connection
 
-    private static final String APPLICATION_NAME = "ECEN689Project1";   // Set the Application Name
-
-    // Directory to store user credentials for this
-    // application.
-    private static final java.io.File DATA_STORE_DIR =
-            new java.io.File(System.getProperty("user.dir"),
-                    ".store/fusion_tables_sample");
-
-    private static FileDataStoreFactory DATA_STORE_FACTORY;             // Global instance of the {@link FileDataStoreFactory}.
-    private static HttpTransport HTTP_TRANSPORT;                        // Global instance of the HTTP transport
-                                                                        // Global instance of the JSON factory.
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static Fusiontables FusionTables;                           // Global instance of the Fusion Tables
-
-    // -----------------------------------------------------------------------------------------------------------------
-    // Create new instances of HTTP_TRANSPORT
-    static                                                              //
-    {                                                                   // and DATA_STORE_FACTORY
-        try                                                             //
-        {                                                               //
-            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-        }                                                               //
-        catch (Throwable t)                                             //
-        {                                                               //
-            t.printStackTrace();                                        //
-            System.exit(1);                                             //
-        }                                                               //
-    }                                                                   //
-
-    //	----------------------------------------------------------------------------------------------------------------
-    //      Method:     authorize
-    //      Inputs:	    none
-    //     Outputs:	    Credentials
-    // Description:     Creates an authorized credential object, will throw IO exception
-    //	----------------------------------------------------------------------------------------------------------------
-    private static Credential authorize() throws IOException
-    {
-        // Load client secrets (from JSON file)
-        InputStream in = Main.class.getResourceAsStream("/client_secret.json");
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets,
-                Collections.singleton(FusiontablesScopes.FUSIONTABLES)).setDataStoreFactory(DATA_STORE_FACTORY).build();
-
-        // Authorize and get the credential
-        Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-        System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-        return credential;                                              // Return the credential
-    }
-
     //	----------------------------------------------------------------------------------------------------------------
     //       Method:    getConnection
     //       Inputs:	database URL, username, password
     //      Outputs:	connection to DB
     //  Description:    Connects to MySql database
     //	----------------------------------------------------------------------------------------------------------------
-    public static Connection getConnection(String dbURL, String user, String password) throws SQLException, ClassNotFoundException
+    private static Connection getConnection(String dbURL, String user, String password) throws SQLException, ClassNotFoundException
     {
         Class.forName("com.mysql.jdbc.Driver");                         // Setup for the MySql JDBC Driver
         Properties props = new Properties();                            // Build the properties
