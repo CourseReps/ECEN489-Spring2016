02edd09cbbbf06c07c71bace097623e9a0bd3f5c
Akash Sahoo <akash.nitr@gmail.com>
2016-03-10 23:26:34 -0600

    Project2 GPS n sensor

---
+++ Students/akashatnitr/Project2/AndroidCode/app/src/main/java/com/example/akash/androidcompass/interfaceA.java
@@ -0,0 +1,10 @@
+package com.example.akash.androidcompass;
+
+import android.hardware.SensorEventListener;
+import android.location.LocationListener;
+
+/**
+ * Created by akash on 3/10/16.
+ */
+public interface interfaceA extends LocationListener, SensorEventListener {
+}
