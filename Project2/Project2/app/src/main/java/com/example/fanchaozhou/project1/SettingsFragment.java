21bc782f0630b5e84d2825e5d0188c28f349a93d
leoyyx2009 <leoyyx2009>
2016-03-22 17:55:49 -0500

    shared pref manage

--- Project2/Project2/app/src/main/java/com/example/fanchaozhou/project1/SettingsFragment.java
+++ Project2/Project2/app/src/main/java/com/example/fanchaozhou/project1/SettingsFragment.java
@@ -9,12 +9,17 @@ package com.example.fanchaozhou.project1;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.HashSet;
 
 /**
  * Created by Fanchao Zhou on 2/21/2016.
@@ -59,12 +64,119 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
     /**
      * @fn onStart
      * @brief The following code in this method is for displaying the values of all the settings whenever the app switches to the settings page
+     *
+     * This method will automatically save the values to com.example.fanchaozhou.project1_preference.xml. The app will reload the setting information saved before.
+     * Clicking reset button will reset everything.
      */
     @Override
     public void onStart() {
         super.onStart();
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        Preference defaultSettings = findPreference("mybutton");
+        //   defaultSettings.setOnPreferenceClickListener(this);
+        defaultSettings.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if (preference.getKey().equals("mybutton")) {
+
+                    EditTextPreference e1;
+                    e1 = (EditTextPreference) findPreference(getString(R.string.pref_http_key));
+                    String s1 = getString(R.string.pref_http_default);
+                    e1.setText(s1);
+                    e1.setSummary(s1);
+                    e1.getText();
+
+                    EditTextPreference e2;
+                    e2 = (EditTextPreference) findPreference(getString(R.string.pref_serial_baudrate_key));
+                    String s2 = getString(R.string.pref_serial_baudrate_default);
+                    e2.setText(s2);
+                    e2.setSummary(s2);
+                    e2.getText();
+
+                    EditTextPreference e3;
+                    e3 = (EditTextPreference) findPreference(getString(R.string.pref_port_numb_key));
+                    String s3 = getString(R.string.pref_port_numb_default);
+                    e3.setText(s3);
+                    e3.setSummary(s3);
+                    e3.getText();
+
+                    EditTextPreference e4;
+                    e4 = (EditTextPreference) findPreference(getString(R.string.pref_s1_id_key));
+                    String s4 = getString(R.string.pref_s1_id_default);
+                    e4.setText(s4);
+                    e4.setSummary(s4);
+                    e4.getText();
+
+                    EditTextPreference e5;
+                    e5 = (EditTextPreference) findPreference(getString(R.string.pref_s2_id_key));
+                    String s5 = getString(R.string.pref_s2_id_default);
+                    e5.setText(s5);
+                    e5.setSummary(s5);
+                    e5.getText();
+
+                    EditTextPreference e7;
+                    e7 = (EditTextPreference) findPreference(getString(R.string.pref_init_table_key));
+                    String s7 = "False";
+                    e7.setText(s7);
+                    e7.setSummary(s7);
+                    e7.getText();
+
+
+                    EditTextPreference e8;
+                    e8 = (EditTextPreference) findPreference(getString(R.string.pref_table_name_key));
+                    String s8 = getString(R.string.pref_table_name_default);
+                    e8.setText(s8);
+                    e8.setSummary(s8);
+                    e8.getText();
+
+
+                    EditTextPreference e9;
+                    e9 = (EditTextPreference) findPreference(getString(R.string.pref_tolerance_theta_key));
+                    String s9 = getString(R.string.pref_tolerance_theta_default);
+                    e9.setText(s9);
+                    e9.setSummary(s9);
+                    e9.getText();
+
+                    EditTextPreference e10;
+                    e10 = (EditTextPreference) findPreference(getString(R.string.pref_tolerance_phi_key));
+                    String s10 = getString(R.string.pref_tolerance_phi_default);
+                    e10.setText(s10);
+                    e10.setSummary(s10);
+                    e10.getText();
+
+                    CheckBoxPreference box1;
+                    box1 = (CheckBoxPreference) findPreference(getString(R.string.pref_gps_changes_key));
+                    box1.setChecked(false);
+
+                    CheckBoxPreference box2;
+                    box2 = (CheckBoxPreference) findPreference(getString(R.string.pref_rss_s1_key));
+                    box2.setChecked(false);
+
+                    CheckBoxPreference box3;
+                    box3 = (CheckBoxPreference) findPreference(getString(R.string.pref_rss_s2_key));
+                    box3.setChecked(false);
+
+                    CheckBoxPreference box4;
+                    box4 = (CheckBoxPreference) findPreference(getString(R.string.pref_gyro_key));
+                    box4.setChecked(false);
+
+                    CheckBoxPreference box5;
+                    box5 = (CheckBoxPreference) findPreference(getString(R.string.pref_mag_key));
+                    box5.setChecked(false);
+
+                    CheckBoxPreference box6;
+                    box6 = (CheckBoxPreference) findPreference(getString(R.string.pref_compass_key));
+                    box6.setChecked(false);
+
+                    MultiSelectListPreference list1;
+                    list1 = (MultiSelectListPreference) findPreference(getString(R.string.pref_json_changes_key));
+                    list1.setValues(new HashSet<String>());
+                }
+                return false;
+            }
+        });//Registering a listener for this item
 
         String serverAddr = sharedPref.getString(getString(R.string.pref_http_key), getString(R.string.pref_http_default));  //Get the server Address
         findPreference(getString(R.string.pref_http_key)).setSummary(serverAddr);
