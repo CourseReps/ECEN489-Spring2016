f8b0c0359979654e2f5325586c52a3053a9e9c41
Keaton Brown <keabro@tamu.edu>
2016-04-26 02:21:30 -0500

    added some comments

--- Isolationbooth/NUC_server/SQLiteJDBC.java
+++ Isolationbooth/NUC_server/SQLiteJDBC.java
@@ -5,6 +5,7 @@ public class SQLiteJDBC
 {
     public static void main(String[] args) 
     {
+        //opening connection to database
         Connection c = null;
         try {
                 Class.forName("org.sqlite.JDBC");
@@ -17,6 +18,7 @@ public class SQLiteJDBC
                 System.err.println(e.getClass().getName() + ": " + e.getMessage());
                 System.exit(1);
             }
+        //continue looping in order to process new database information
         while (true) 
         {
 
@@ -25,15 +27,13 @@ public class SQLiteJDBC
             String mac = ""; //mac address of max rssi
             
             Statement stmt = null;
-            ResultSet numretwlan0 = null;
-            ResultSet numretwlan1 = null;
-            ResultSet numretwlan2 = null;
+            ResultSet numretwlan0 = null; //resultset for rows in table wlan0
+            ResultSet numretwlan1 = null; //resultset for rows in table wlan1
+            ResultSet numretwlan2 = null; //resultset for rows in table wlan2
             int rowCount = 0;
             int rowCount1 = 0;
             int rowCount2 = 0;
-            //opening database
             
-
             try{
                 stmt = c.createStatement();
 
@@ -60,14 +60,14 @@ public class SQLiteJDBC
                 rowCount2 = numretwlan2.getInt(1);
                 System.out.println(rowCount2);
                 numretwlan2.close();
+
             }catch(Exception e){
                 System.err.println(e);
             }
                 
             //getting number of entries in table for iteration
-           /* try{
-                long unixTime = System.currentTimeMillis() / 1000L;
-                //iterating through tables to find max rssi
+           try{
+                long unixTime = System.currentTimeMillis() / 1000L; //current time for comparison
                     int rssiwlan0 = 0;
                     int rssiwlan1 = 0;
                     int rssiwlan2 = 0;
@@ -76,17 +76,16 @@ public class SQLiteJDBC
                     
                     String query = "SELECT * FROM wlan0"; 
                     ResultSet rssi0 = s1.executeQuery(query);
-                        
+                    //iterate through result set to find old entries to delete
                     while(rssi0.next()){
                         int timestamp = rssi0.getInt(3);
-                        System.out.println(timestamp);
+
                         if(unixTime-timestamp > 120){
                             String delete = "DELETE FROM wlan0 WHERE Timestamp = ?";
                             PreparedStatement time = c.prepareStatement(delete);
                             time.setInt(1,timestamp);
                             time.execute();
                             time.close();
-                            System.out.println(delete);
                         }
                     }
                     rssi0.close();
@@ -94,6 +93,7 @@ public class SQLiteJDBC
 
                     query = "SELECT * FROM wlan1";
                     ResultSet rssi1 = s1.executeQuery(query);
+                    //iterate through result set to find old entries to delete
                     while(rssi1.next())
                     {
                         int timestamp = rssi1.getInt(3);
@@ -104,13 +104,13 @@ public class SQLiteJDBC
                             time.setInt(1,timestamp);
                             time.execute();
                             time.close();
-                            System.out.println(delete);
                         }
                     }
                     rssi1.close();
 
                     query = "SELECT * FROM wlan2";
                     ResultSet rssi2 = s1.executeQuery(query);
+                    //iterate through result set to find old entries to delete
                     while(rssi2.next()){
                         int timestamp = rssi2.getInt(3);
 
@@ -120,7 +120,6 @@ public class SQLiteJDBC
                             time.setInt(1,timestamp);
                             time.execute();
                             time.close();
-                            System.out.println(delete);
                         }
                     }
                     rssi2.close();
@@ -129,12 +128,11 @@ public class SQLiteJDBC
             }catch(Exception e){
                     System.err.println(e);
                     System.exit(2);
-                }*/
+                }
 
-        try{
+            try{
                 String query = "SELECT Mac_Address FROM wlan0";
                 ResultSet macrs = stmt.executeQuery(query);
-                long unixTime = System.currentTimeMillis() / 1000L;
 
                 //iterating through tables to find max rssi
                 while (macrs.next()) 
@@ -143,7 +141,7 @@ public class SQLiteJDBC
                     int rssiwlan1 = 0;
                     int rssiwlan2 = 0;
 
-                    String macst = macrs.getString(1);
+                    String macst = macrs.getString(1); //iterates through each mac_address
                     Statement s1 = c.createStatement();
                     query = "SELECT * FROM wlan0"; 
                     ResultSet rssi0 = s1.executeQuery(query);
@@ -151,8 +149,8 @@ public class SQLiteJDBC
                     while(rssi0.next())
                     {
                             String macwlan0 = rssi0.getString(2);
-                            System.out.println(macwlan0);
-                            if(macwlan0.equals(macst))
+
+                            if(macwlan0.equals(macst))  //compares with mac address in first loop if the same get rssi
                                 rssiwlan0 = rssi0.getInt(1);
                     }
                     rssi0.close();
@@ -163,8 +161,8 @@ public class SQLiteJDBC
                     while(rssi1.next())
                     {
                         String macwlan1 = rssi1.getString(2);
-                        System.out.println(macwlan1);
-                            if(macwlan1.equals(macst))
+
+                            if(macwlan1.equals(macst)) //compares with mac address in first loop if the same get rssi
                                 rssiwlan1 = rssi1.getInt(1);
                     }
                     rssi1.close();
@@ -175,36 +173,34 @@ public class SQLiteJDBC
                     while(rssi2.next())
                     {
                         String macwlan2 = rssi2.getString(2);
-                        System.out.println(macwlan2);
-                            if(macwlan2.equals(macst))
+                            if(macwlan2.equals(macst)) //compares with mac address in first loop if the same get rssi
                                 rssiwlan2 = rssi2.getInt(1);
                     }
                     rssi2.close();
                     s1.close();
 
-                    double rssimag = sqrt((rssiwlan0 * rssiwlan0) + (rssiwlan1 * rssiwlan1) + (rssiwlan2 * rssiwlan2));
-                    avg.put(rssimag,macst);
+                    double rssimag = sqrt((rssiwlan0 * rssiwlan0) + (rssiwlan1 * rssiwlan1) + (rssiwlan2 * rssiwlan2)); //magnitude of rssi
+                    avg.put(rssimag,macst); //adds rssi and associated MAC address to map
 
-                    System.out.println(macst);
                 }
                 macrs.close();
             }catch (Exception e) {
                 System.err.println(e);
                 System.exit(2);
             }
-      
-            for (Map.Entry<Double, String> entry : avg.entrySet()) 
-            {
+            //iterates through map to find maximum rssi and associated MAC address
+            for (Map.Entry<Double, String> entry : avg.entrySet()){
                 double rssiVal = entry.getKey();
+
                 if(rssiVal > rssimax)
                     rssimax=rssiVal;
             }
             mac = avg.get(rssimax);
 
-            if(rssimax < 40)
+            if(rssimax < 40) //threshold for device to be considered within the area
             {
             	try{
-            	System.out.println("Executing picture request");
+            	System.out.println("Executing picture request");  //runs get picture from android camera program
                 Runtime.getRuntime().exec("java client");
                 }catch(Exception e){
                     System.err.println(e);
@@ -212,12 +208,12 @@ public class SQLiteJDBC
                 }
             }
             System.out.println("pic finished");
-            /*System.out.println(mac);
-            avg.clear();
-*/
+            System.out.println(mac);
+            System.out.println(rssimax);
+            avg.clear(); //clears map for next loop
+
             try{
 	            stmt.close();
-                //c.close();
                 Thread.sleep(1000);
             }catch(Exception e) {
                 System.err.println(e);
